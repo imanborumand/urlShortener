98dd3b26681deab64b11c54bfa9f06d7498dbb81
Iman Borumand Zadeh <imanborumand@gmail.com>
2023-01-03 07:08:16 +0000

    fixed- fix CORS

--- app/index.php
+++ app/index.php
@@ -8,12 +8,15 @@ require __DIR__ . '/src/config/lang.php';
 
 
 ini_set('display_errors', DISPLAY_DEBUG);
+
+
 header("Content-Type:application/json");
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');    // cache for 1 day
+
 
 define( "DEFAULT_LANGUAGE" , $lang[$config['lang']] ); //set default language
 
-
+//print_r(json_decode(file_get_contents('php://input'), true));exit;
 require __DIR__ . '/src/routers/api.php';
